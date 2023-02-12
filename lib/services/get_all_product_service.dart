79fc8a92da46c38caa87e0eeebdb85bd1095133e
Faisal <fnmghaithi@gmail.com>
2023-02-12 18:05:09 +0400

    Finished AllProductService.

--- lib/services/get_all_product_service.dart
+++ lib/services/get_all_product_service.dart
@@ -1,1 +1,24 @@
-class GetAllProducts {}
+import 'dart:convert';
+import 'package:store_app/models/product.dart';
+import 'package:http/http.dart' as http;
+
+class AllProductsService {
+  Future<List<Product>> getAllProducts() async {
+    Uri url = Uri.parse('https://fakestoreapi.com/products');
+    http.Response response = await http.get(url);
+    List<dynamic> data = jsonDecode(response.body);
+    List<Product> productsList = [];
+
+    for (dynamic jsonData in data) {
+      productsList.add(
+        Product.fromJson(jsonData),
+      );
+    }
+    // for (int i = 0; i < data.length; i++) {
+    //   productsList.add(
+    //     Product.fromJson(data[i]),
+    //   );
+    // }
+    return productsList;
+  }
+}
