a9cece7cd883cff8a7f58d8ad3e84f4eeb778fb8
Faisal <fnmghaithi@gmail.com>
2023-02-16 14:10:20 +0400

    Started with the ProductCard UI.

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatelessWidget {
+  static const String id = 'HomePage';
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          'New Trend',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.cartPlus,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 40.0,
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 0,
+              offset: const Offset(10, 10),
+            ),
+          ],
+        ),
+        height: 130,
+        width: 220,
+        child: Card(
+          elevation: 10.0,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 16.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Handbag LV',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '\$200',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
