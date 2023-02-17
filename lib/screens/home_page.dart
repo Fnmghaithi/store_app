d33a7bf1df4b2c8cb203b16bca33cfc7c41cbe04
Faisal <fnmghaithi@gmail.com>
2023-02-17 14:31:38 +0400

    Used Stack and Positioned widgets.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -28,57 +28,70 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 40.0,
-              color: Colors.grey.withOpacity(0.2),
-              spreadRadius: 0,
-              offset: const Offset(10, 10),
-            ),
-          ],
-        ),
-        height: 130,
-        width: 220,
-        child: Card(
-          elevation: 10.0,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 16.0,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'Handbag LV',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 16,
-                  ),
+      body: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 40.0,
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 0,
+                  offset: const Offset(10, 10),
                 ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ],
+            ),
+            height: 130,
+            width: 220,
+            child: Card(
+              elevation: 10.0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 16.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Text(
-                      '\$200',
+                      'Handbag LV',
                       style: TextStyle(
+                        color: Colors.grey,
                         fontSize: 16,
                       ),
                     ),
-                    Icon(
-                      Icons.favorite,
-                      color: Colors.red,
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '\$200',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
           ),
-        ),
+          Positioned(
+            right: 32,
+            top: -65,
+            child: Image.network(
+              'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+              height: 100,
+            ),
+          ),
+        ],
       ),
     );
   }
